e5661bbe0dd2a7e2717db900f3dc690b6cb02cde
vinders <red-wind@hotmail.com>
2016-08-18 03:32:46 +0200

    config dialog resources

--- pandoraGS_v0.2/PandoraGS/resource.h
+++ pandoraGS_v0.2/PandoraGS/resource.h
@@ -8,6 +8,81 @@
 #define IDD_ABOUT_DIALOG                105
 #define IDC_ABOUT_TECH                  106
 
+#define IDD_CONFIG_DIALOG               110
+#define IDC_LANG_LIST                   111
+#define IDD_GENERAL_PAGE                120
+#define IDD_MANAGER_PAGE                121
+#define IDD_PROFILE_PAGE                122
+#define IDD_PROFILE_FILTERS_TAB         123
+#define IDD_PROFILE_STRETCHING_TAB      124
+#define IDD_PROFILE_FIXES_TAB           125
+#define IDD_KEYBINDING_DIALOG           130
+#define IDD_ADVANCED_DIALOG             131
+#define IDD_ADDPROFILE_DIALOG           132
+#define IDD_IMPORT_DIALOG               133
+#define IDD_EXPORT_DIALOG               134
+
+#define IDS_GEN_GROUP1                  201
+#define IDS_GEN_RESOLUTION              202
+#define IDC_GEN_FULLRES                 203
+#define IDC_GEN_FULLRES_LIST            204
+#define IDC_GEN_WINRES                  205
+#define IDC_GEN_WINRESX_EDIT            206
+#define IDC_GEN_WINRESY_EDIT            207
+#define IDC_GEN_WINSIZE_CHECK           208
+#define IDS_GEN_COLOR                   209
+#define IDC_GEN_COLOR_LIST              210
+#define IDC_GEN_GTEACC_CHECK            211
+#define IDC_GEN_GTEACC_LIST             212
+#define IDS_GEN_GROUP2                  220
+#define IDC_GEN_FRAMESKIP_CHECK         221
+#define IDC_GEN_FRAMELIMIT_CHECK        222
+#define IDC_GEN_FPSAUTO_RADIO           223
+#define IDC_GEN_FPSFIXED_RADIO          224
+#define IDC_GEN_FRAMELIMIT_EDIT         225
+#define IDS_GEN_FPSUNIT                 226
+#define IDC_GEN_SCNSAVER_CHECK          230
+#define IDC_GEN_BTN_KEYBINDING          231
+#define IDC_GEN_BTN_ADVANCED            232
+
+#define IDC_MNG_BTN_ADD                 310
+#define IDC_MNG_BTN_REMOVE              311
+#define IDC_MNG_BTN_IMPORT              312
+#define IDC_MNG_BTN_EXPORT              313
+#define IDC_MNG_PRESETS                 314
+#define IDC_MNG_PRESETS_LIST            315
+#define IDC_MNG_BTN_PRESETS             316
+
+#define IDS_PRO_GROUP1                  401
+#define IDS_PRO_TXFILTER                402
+#define IDC_PRO_TXFILTER_LIST           403    
+#define IDC_PRO_TXUPSCALE_LIST          404
+#define IDS_PRO_2DFILTER                405
+#define IDC_PRO_2DFILTER_LIST           406
+#define IDC_PRO_2DUPSCALE_LIST          407
+#define IDS_PRO_SCRFILTER               408
+#define IDC_PRO_SCRFILTER_LIST          409
+#define IDC_PRO_MDEC_CHECK              410
+#define IDC_PRO_FXAA_CHECK              420
+#define IDC_PRO_AAFILTER_LIST           421
+
+#define IDS_PROSTR_INTRES               501
+#define IDC_PROSTR_INTRESX_LIST         502
+#define IDC_PROSTR_INTRESY_LIST         503
+#define IDS_PROSTR_STRETCH_MIN          504
+#define IDC_PROSTR_STRETCH_SLIDER       505
+#define IDS_PROSTR_STRETCH_MAX          506
+#define IDS_PROSTR_CUT_MIN              507    
+#define IDC_PROSTR_CUT_SLIDER           508
+#define IDS_PROSTR_CUT_MAX              509
+#define IDS_PROSTR_RATIO                510
+#define IDC_PROSTR_RATIO_LIST           511
+#define IDC_PROSTR_MIRROR               513
+#define IDC_PROSTR_BLACKBORDERS_CHECK   514
+#define IDC_PROSTR_BLACKBORDERS_EDIT    516
+#define IDS_PROSTR_BLACKBORDERS_UNIT    517
+#define IDC_PROSTR_CURVATURE_CHECK      518
+#define IDC_PROSTR_CURVATURE_LIST       519
 
 // Next default values for new objects
 // 
