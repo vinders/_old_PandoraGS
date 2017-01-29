700ca34be4a0ef072b9c6f4fbc3d00e6ce9ba624
vinders <red-wind@hotmail.com>
2017-01-29 18:02:12 +0100

    refactoring - new language management

--- project/src/lang/menu_lang.cpp
+++ project/src/lang/menu_lang.cpp
@@ -6,3 +6,63 @@ License :     GPLv2
 ------------------------------------------------------------------------
 Description : in-game menu language/translation utility
 *******************************************************************************/
+#include "../globals.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "../events/utils/logger.h"
+#include "../events/utils/std_exception.hpp"
+#include "menu_lang.h"
+using namespace lang;
+
+
+/// @brief Set english values
+void MenuLang::setLanguageEnglish()
+{
+
+}
+
+/// @brief Set spanish values
+void MenuLang::setLanguageSpanish()
+{
+
+}
+
+/// @brief Set french values
+void MenuLang::setLanguageFrench()
+{
+
+}
+
+/// @brief Set german values
+void MenuLang::setLanguageGerman()
+{
+
+}
+
+/// @brief Read values from file (english if not found)
+/// @param filePath Language file path
+void MenuLang::setLanguageFromFile(std::wstring& filePath)
+{
+    setLanguageEnglish(); // default values
+
+    try
+    {
+        // open language file
+        std::ifstream in;
+        in.open(filePath, std::ifstream::in);
+        if (!in.is_open())
+        {
+            events::utils::Logger::getInstance()->writeErrorEntry("MenuLang.setLanguageFromFile", "File not found or not accessible.");
+            return;
+        }
+
+        //...read file
+
+        in.close();
+    }
+    catch (const std::exception& exc) // in-game -> no exception
+    {
+        events::utils::Logger::Logger::getInstance()->writeErrorEntry("MenuLang.setLanguageFromFile", exc.what());
+    }
+}
--- project/src/lang/menu_lang.h
+++ project/src/lang/menu_lang.h
@@ -9,6 +9,7 @@ Description : in-game language/translation utility
 #pragma once
 
 #include <string>
+#include "i_lang.h"
 
 /// @namespace lang
 /// Language utilities
@@ -16,8 +17,33 @@ namespace lang
 {
     /// @class MenuLang
     /// @brief Translation utility for in-game plugin menu
-    class MenuLang
+    class MenuLang : public ILang
     {
-        
+    public:
+        //std::wstring ...
+
+    public:
+        /// @brief Create uninitialized language resource (for in-game menu)
+        MenuLang() {}
+        /// @brief Create language resource (for in-game menu)
+        /// @param code Language code
+        /// @param filePath Language file path
+        MenuLang(langcode_t code, std::wstring& filePath)
+        {
+            setLanguage(code, filePath);
+        }
+
+        /// @brief Set english values
+        virtual void setLanguageEnglish();
+        /// @brief Set spanish values
+        virtual void setLanguageSpanish();
+        /// @brief Set french values
+        virtual void setLanguageFrench();
+        /// @brief Set german values
+        virtual void setLanguageGerman();
+
+        /// @brief Read values from file (english if not found)
+        /// @param filePath Language file path
+        virtual void setLanguageFromFile(std::wstring& filePath);
     };
 }
