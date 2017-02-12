e1b8f0ea1208a876042c00c7e0a713f66f877541
vinders <red-wind@hotmail.com>
2017-02-12 20:11:52 +0100

    refactoring - add some language resources

--- project/src/lang/config_lang.cpp
+++ project/src/lang/config_lang.cpp
@@ -19,25 +19,45 @@ using namespace lang;
 /// @brief Set english values
 void ConfigLang::setLanguageEnglish() noexcept
 {
-
+    dialog.confirm = L"OK"s;
+    dialog.cancel  = L"Cancel"s;
+    dialog.generalSettings   = L"General settings"s;
+    dialog.profileManagement = L"Profile manager"s;
+    dialog.profileSettings   = L"Profile settings"s;
+    profile.profileList = L"Current profile:"s;
+    profile.filteringTab = L"Filters"s;
+    profile.screenTab = L"Display and ratio"s;
+    profile.compatibilityTab = L"Compatibility settings"s;
+    
+    //...
 }
 
 /// @brief Set spanish values
 void ConfigLang::setLanguageSpanish() noexcept
 {
-
+    /*...to do...*/setLanguageEnglish();
 }
 
 /// @brief Set french values
 void ConfigLang::setLanguageFrench() noexcept
 {
-
+    dialog.confirm = L"OK"s;
+    dialog.cancel  = L"Annuler"s;
+    dialog.generalSettings   = L"Options générales"s;
+    dialog.profileManagement = L"Gestion de profils"s;
+    dialog.profileSettings   = L"Paramétrer profil"s;
+    profile.profileList = L"Profil courant :"s;
+    profile.filteringTab = L"Filtres"s;
+    profile.screenTab = L"Affichage et ratio"s;
+    profile.compatibilityTab = L"Options de compatibilité"s;
+    
+    //...
 }
 
 /// @brief Set german values
 void ConfigLang::setLanguageGerman() noexcept
 {
-
+    /*...to do...*/setLanguageEnglish();
 }
 
 /// @brief Read values from file (error if not found)
