1b526cdcaec15d2405c019dcf99e5421434be498
vinders <red-wind@hotmail.com>
2017-05-07 22:00:26 +0200

    move opengl vendor files

--- project/src/vendor/opengl.h
+++ project/src/vendor/opengl.h
@@ -1,6 +1,4 @@
 /*******************************************************************************
-PANDORAGS project - PS1 GPU driver
-------------------------------------------------------------------------
 Description : opengl specific includes
 *******************************************************************************/
 // dependencies : opengl32.lib;glu32.lib; 
@@ -9,9 +7,9 @@ Description : opengl specific includes
 #ifdef _WINDOWS
 #define GLEW_STATIC // static linking    (warning : constant also defined in glew.h)
 #define GLEW_BUILD  // build DLL library (warning : constant also defined in glew.h)
-#include "glew.h"   // 1.9.0
-#include "wglew.h"
+#include "opengl/glew.h"   // 1.9.0
+#include "opengl/wglew.h"
 #else
-#include "glew.h"   // 1.9.0
+#include "opengl/glew.h"   // 1.9.0
 #include <GLFW/glfw3.h> // glfw3.lib
 #endif
