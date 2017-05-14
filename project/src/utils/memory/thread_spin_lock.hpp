abf2995650e7040acf65eec9e51010e7ca02ddd2
vinders <red-wind@hotmail.com>
2017-05-14 13:47:44 +0200

    add spin-lock re-lockable by same thread without deadlock

--- project/src/utils/memory/thread_spin_lock.hpp
+++ project/src/utils/memory/thread_spin_lock.hpp
@@ -2,7 +2,7 @@
 Author  :     Romain Vinders
 License :     GPLv2
 ------------------------------------------------------------------------
-Description : spin-lock (no multi-lock) : very fast mutex (if no wait -> only use if rare concurrency)
+Description : spin-lock with multi-lock per thread : very fast mutex (if no wait -> only use if rare concurrency)
 *******************************************************************************/
 #pragma once
 
@@ -11,6 +11,7 @@ Description : spin-lock (no multi-lock) : very fast mutex (if no wait -> only us
 #include <thread>
 #include <atomic>
 #include <chrono>
+#include <thread>
 
 /// @namespace utils
 /// General utilities
@@ -20,16 +21,16 @@ namespace utils
     /// Memory management utilities
     namespace memory
     {
-        /// @class SpinLock
-        /// @brief Spin-lock mutex
-        class SpinLock
+        /// @class ThreadSpinLock
+        /// @brief Spin-lock mutex (locked once per thread)
+        class ThreadSpinLock
         {
         public:
             /// @brief Create unlocked instance
             SpinLock() : m_status(ATOMIC_FLAG_INIT) {}
             /// @brief Move instance
             /// @param[in] other  Other instance to move
-            explicit SpinLock(SpinLock&& other) : m_status(other.m_status) {}
+            explicit SpinLock(SpinLock&& other) : m_status(other.m_status), m_lockThreadId(other.m_lockThreadId) {}
             // no copy allowed
             SpinLock(const SpinLock& other) = delete;
             
@@ -57,16 +58,30 @@ namespace utils
             /// @brief Wait until instance is unlocked, then lock it
             inline void lock() noexcept
             {
+                std::thread::id threadId(std::this_thread::get_id());
+                if (threadId == m_lockThreadId) // same thread -> increase lock count
+                {
+                    ++m_lockCount;
+                    return;
+                }
+                
                 while (m_status.test_and_set(std::memory_order_acquire))
                 {
                     std::this_thread::yield();
                 }
+                m_lockThreadId = threadId;
             }
             /// @brief Wait until instance unlocked or timeout
-            /// @param[in] timeout  Timeout (milliseconds)
             /// @returns Success (true) or timeout
             inline bool lock(uint32_t timeout) noexcept
             {
+                std::thread::id threadId(std::this_thread::get_id());
+                if (threadId == m_lockThreadId) // same thread -> increase lock count
+                {
+                    ++m_lockCount;
+                    return true;
+                }
+                
                 if (m_status.test_and_set(std::memory_order_acquire)) // if already locked
                 {
                     // set timeout reference
@@ -82,23 +97,56 @@ namespace utils
                         std::this_thread::yield();
                     }
                 }
+                m_lockThreadId = threadId;
                 return true;
             }
             /// @brief Try to lock, if not already locked
             /// @returns Success (true) or not
             inline bool tryLock() noexcept
             {
-                return !m_status.test_and_set(std::memory_order_acquire); // return opposite of previous value
+                std::thread::id threadId(std::this_thread::get_id());
+                if (threadId == m_lockThreadId) // same thread -> increase lock count
+                {
+                    ++m_lockCount;
+                    return true;
+                }
+                
+                if (!m_status.test_and_set(std::memory_order_acquire)) // return opposite of previous value
+                {
+                    m_lockThreadId = threadId;
+                    return true;
+                }
+                return false;
             }
-            /// @brief Unlock instance
+            
+            /// @brief Unlock instance if locked by same thread - prefered method if lock/unlock are not called in same function
+            /// @returns Success (same thread and currently in locked state) or not
+            inline bool safeUnlock() noexcept
+            {
+                if (std::this_thread::get_id() != m_lockThreadId) // different thread or unlocked
+                    return false;
+                unlock();
+                return true;
+            }
+            /// @brief Unlock instance without checking thread ID - safe if lock/unlock are called in same function
             inline void unlock() noexcept
             {
-                m_status.clear(std::memory_order_release);
+                if (m_lockCount == 0u)
+                {
+                    m_lockThreadId = std::thread::id();
+                    m_status.clear(std::memory_order_release);
+                }
+                else
+                {
+                    --m_lockCount;
+                }
             }
             
 
         private:
-            std::atomic_flag m_status;  ///< Lock status (true = locked)
+            std::thread::id m_lockThreadId; ///< Locking thread ID
+            std::atomic_flag m_status;      ///< Lock status (true = locked)
+            uint32_t m_lockCount;           ///< Number of locks made by same thread (if multiple locks, multiple unlocks required)
         };
     }
 }
