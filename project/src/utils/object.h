8e99ebedbe0fedab8258c4864ad117e139f9f5a1
vinders <red-wind@hotmail.com>
2017-06-20 22:46:08 +0200

    utils - object toolset - add type verifications

--- project/src/utils/object.h
+++ project/src/utils/object.h
@@ -8,6 +8,7 @@ Description : object toolset
 
 #include <cstdio>
 #include <cstddef>
+#include <type_traits>
 
 /// @namespace utils
 /// General utilities
@@ -23,6 +24,12 @@ namespace utils
         /// @returns Null or not
         template <typename T>
         static inline bool isNull(const T* pObject) { return (pObject == nullptr); }
+
+        /// @brief Get size of an object (bytes)
+        /// @param[in] obj  Object reference
+        /// @returns Size (bytes)
+        template <typename T>
+        static inline size_t getSize(const T& obj) { return sizeof(obj); }
         
         /// @brief Check whether an object is an instance of a specific child class
         /// @param[in] pObject  Object pointer
@@ -30,10 +37,43 @@ namespace utils
         template <typename ClassType, typename T>
         static inline bool isInstanceOf(const T* pObject) { return (dynamic_cast<ClassType*>(pObject) != nullptr); }
         
-        /// @brief Get size of an object (bytes)
-        /// @param[in] pObject  Object pointer
-        /// @returns Size (bytes)
+        
+        /// @brief Check whether an object is a number
+        /// @param[in] obj  Object reference
+        /// @returns Numeric or not
         template <typename T>
-        static inline size_t getSize(const T* pObject) { return (pObject != nullptr) ? sizeof(*pObject) : 0; }
+        static inline bool isNumeric(const T& obj) { return (std::is_numeric<T>::value); }
+        /// @brief Check whether an object is an integer number
+        /// @param[in] obj  Object reference
+        /// @returns Integer or not
+        template <typename T>
+        static inline bool isInteger(const T& obj) { return (std::is_integral<T>::value); }
+        /// @brief Check whether an object is a floating-point number
+        /// @param[in] obj  Object reference
+        /// @returns Float or not
+        template <typename T>
+        static inline bool isFloat(const T& obj) { return (std::is_floating_point<T>::value); }
+        /// @brief Check whether an object is a signed number
+        /// @param[in] obj  Object reference
+        /// @returns Signed or not
+        template <typename T>
+        static inline bool isSigned(const T& obj) { return (std::is_signed<T>::value); }
+
+        /// @brief Check whether an object is an enumeration item
+        /// @param[in] obj  Object reference
+        /// @returns Enum or not
+        template <typename T>
+        static inline bool isEnum(const T& obj) { return (std::is_enum<T>::value); }
+        /// @brief Check whether an object is a pointer
+        /// @param[in] obj  Object reference
+        /// @returns Pointer or not
+        template <typename T>
+        static inline bool isPointer(const T& obj) { return (std::is_pointer<T>::value); }
+        /// @brief Check whether an object reference is an abstract super-class
+        /// @param[in] obj  Object reference
+        /// @returns Abstract or not
+        template <typename T>
+        static inline bool isAbstract(const T& obj) { return (std::is_abstract<T>::value); }
+        
     };
 }
