28d954d53addc5bf029d4b77f890b7afd7a1c060
vinders <red-wind@hotmail.com>
2017-07-08 20:04:16 +0200

    utils - system ansi color codes - fix linux version

--- project/src/utils/system/ansi_color.h
+++ project/src/utils/system/ansi_color.h
@@ -63,21 +63,59 @@ namespace utils
 #           endif  
         };
         
+        
         /// @brief Change terminal output color
         template <ansi_color_t ForeColor, ansi_color_t BackColor> 
         inline void setTextAnsiColor()
         {
 #           ifdef _WINDOWS
             SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), static_cast<uint16_t>(ForeColor) | (static_cast<uint16_t>(BackColor) << 4));
+            
 #           else // -- linux/UNIX/POSIX --
-            char buffer[] = "\x1B[30;97m";
-            buffer[2] = (BackColor / 10) + '1';
-            buffer[3] = (BackColor % 10) + '0';
-            buffer[5] = (ForeColor / 10) + '1';
-            buffer[6] = (ForeColor % 10) + '0';
-            printf(buffer);
+#           define _UTILS__ANSI_COLOR_CODE_BUILDER(foreColorCode, backColorEnum) \
+                    switch (backColorEnum) \
+                    { \
+                        case black:   printf("\x1B["#foreColorCode";40m"); break; \
+                        case red:     printf("\x1B["#foreColorCode";41m"); break; \
+                        case green:   printf("\x1B["#foreColorCode";42m"); break; \
+                        case yellow:  printf("\x1B["#foreColorCode";43m"); break; \
+                        case blue:    printf("\x1B["#foreColorCode";44m"); break; \
+                        case magenta: printf("\x1B["#foreColorCode";45m"); break; \
+                        case cyan:    printf("\x1B["#foreColorCode";46m"); break; \
+                        case white:   printf("\x1B["#foreColorCode";47m"); break; \
+                        case light_black:   printf("\x1B["#foreColorCode";100m"); break; \
+                        case light_red:     printf("\x1B["#foreColorCode";101m"); break; \
+                        case light_green:   printf("\x1B["#foreColorCode";102m"); break; \
+                        case light_yellow:  printf("\x1B["#foreColorCode";103m"); break; \
+                        case light_blue:    printf("\x1B["#foreColorCode";104m"); break; \
+                        case light_magenta: printf("\x1B["#foreColorCode";105m"); break; \
+                        case light_cyan:    printf("\x1B["#foreColorCode";106m"); break; \
+                        case light_white:   printf("\x1B["#foreColorCode";107m"); break; \
+                    }
+
+            switch (ForeColor)
+            {
+                case black:   { _UTILS__ANSI_COLOR_CODE_BUILDER(30, BackColor); break; }
+                case red:     { _UTILS__ANSI_COLOR_CODE_BUILDER(31, BackColor); break; }
+                case green:   { _UTILS__ANSI_COLOR_CODE_BUILDER(32, BackColor); break; }
+                case yellow:  { _UTILS__ANSI_COLOR_CODE_BUILDER(33, BackColor); break; }
+                case blue:    { _UTILS__ANSI_COLOR_CODE_BUILDER(34, BackColor); break; }
+                case magenta: { _UTILS__ANSI_COLOR_CODE_BUILDER(35, BackColor); break; }
+                case cyan:    { _UTILS__ANSI_COLOR_CODE_BUILDER(36, BackColor); break; }
+                case white:   { _UTILS__ANSI_COLOR_CODE_BUILDER(37, BackColor); break; }
+                case light_black:   { _UTILS__ANSI_COLOR_CODE_BUILDER(90, BackColor); break; }
+                case light_red:     { _UTILS__ANSI_COLOR_CODE_BUILDER(91, BackColor); break; }
+                case light_green:   { _UTILS__ANSI_COLOR_CODE_BUILDER(92, BackColor); break; }
+                case light_yellow:  { _UTILS__ANSI_COLOR_CODE_BUILDER(93, BackColor); break; }
+                case light_blue:    { _UTILS__ANSI_COLOR_CODE_BUILDER(94, BackColor); break; }
+                case light_magenta: { _UTILS__ANSI_COLOR_CODE_BUILDER(95, BackColor); break; }
+                case light_cyan:    { _UTILS__ANSI_COLOR_CODE_BUILDER(96, BackColor); break; }
+                case light_white:   { _UTILS__ANSI_COLOR_CODE_BUILDER(97, BackColor); break; }
+            }
+#           undef _UTILS__ANSI_COLOR_CODE_BUILDER
 #           endif  
         }
+        
         
         /// @brief Change terminal text output color and print some text
         /// @param[in] format     Format string (printf compatible)
