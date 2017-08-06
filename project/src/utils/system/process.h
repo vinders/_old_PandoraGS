e14d24b79d349bb71deca354b2da6b38b4a32c25
vinders <red-wind@hotmail.com>
2017-08-06 14:10:28 +0200

    utils - process management - declarations + docs

--- project/src/utils/system/process.h
+++ project/src/utils/system/process.h
@@ -22,36 +22,103 @@ namespace utils
         class Process
         {
         public:
-            Process(const std::string& path);
-            Process(const Process& other) = delete;
+            /// @brief Create a child process by launching a specific executable
+            /// @param[in] filePath  Path to the executable file
+            Process(const std::string& filePath);
+            /// @brief Move an instance
+            /// @param[in] other  Other instance
             Process(Process&& other);
-            ~Process();
+            // no copy allowed
+            Process(const Process& other) = delete;
             
+            /// @brief Directly terminate the child process (unless it's been detached or interrupted) and remove it from process table
+            /// @warning It is recommended to aither call interrupt() (to kill it softly in a manageable way) or detach() before the destructor is called
+            ~Process()
+            {
+                if (m_isDetached == false)
+                    destroy();
+            }
+            
+            // no copy assignment allowed
             Process& operator=(const Process& other) = delete;
-            Process& operator=(Process&& other);
+            // no move assignment allowed
+            Process& operator=(Process&& other) = delete;
             
             
+            // -- Child process info --
+            
+            /// @brief Get child process identifier (system specific)
+            /// @returns Process ID or handle
             uint32_t getProcessId();
+            /// @brief Check if the child process is still running
+            /// @returns Running or exited
             bool isRunning();
             
+            /// @brief Get child process priority
+            /// @returns Process priority value
             uint32_t getPriority();
+            /// @brief Change child process priority
+            /// @param[in] priority  Process priority value
             void setPriority(const uint32_t& priority);
             
             
-            static Process fork();
+            // -- Main/parent process info --
             
+            /// @brief Get main process priority
+            /// @returns Process priority value
+            uint32_t getMainProcessPriority();
+            /// @brief Change main process priority
+            /// @param[in] priority  Process priority value
+            void setMainProcessPriority(const uint32_t& priority);
+            
+            
+            // -- Child process operations --
+            
+            /// @brief Detach the child process : make it able to live without the parent process
+            /// @warning The process won't be terminated by destructor + no need (and no possibility) to wait for it to exit anymore
             void detach();
-            static void waitForExit(const Process& process);
-            static void waitForAnyChildExit();
             
+            /// @brief Send interruption signal to child process (will softly terminate it, can be handled by the child process if necessary)
+            /// @warning This call does not wait, and thus, the process won't be removed from the process table when it finally decides to exit (call waitForExit for that)
             void interrupt();
+            /// @brief Send termination signal to child process (will immediately terminate it, can't be handled) and remove it from process table (no need for waitForExit)
+            /// @warning Unlike interrupt(), the process is removed from the process table, because there's no need to wait (the process is directly terminated)
             void destroy();
             
+            
+            // -- Main/parent process operations --
+            
+            /// @brief Clone main process and its context - a child process will run from the same position as the parent
+            /// @returns Management object for child process
+            static Process fork();
+            
+            /// @brief Wait for a specific child process to exit and/or remove zombie-process from process table
+            /// @warning If the process has already been removed from the process table, this function will do nothing
+            static void waitForExit(const Process& process);
+            /// @brief Wait for a specific child process to exit and/or remove zombie-process from process table
+            /// @param[in] timeout  Maximum wait (milliseconds)
+            /// @returns True if a child process has exited, or false if timeout
+            /// @warning If the process has already been removed from the process table, this function will do nothing
+            static void waitForExit(const Process& process, const uint32_t timeout);
+            
+            /// @brief Wait until any child process exits and becomes a zombie-process, then remove it from process table
+            /// @warning Will wait unconditionally, even if no child process exists (and will be stuck forever in that case).
+            static void waitForAnyChildExit();
+            /// @brief Wait until any child process exits and becomes a zombie-process, then remove it from process table
+            /// @param[in] timeout  Maximum wait (milliseconds)
+            /// @returns True if a child process has exited, or false if timeout
+            /// @warning Will wait unconditionally until the timeout occurs
+            static bool waitForAnyChildExit(const uint32_t timeout);
+            
+            
         protected:
-            Process();
+            /// @brief Create empty instance with no execution (used by operations such as fork)
+            /// @warning Member m_isDetached should be set to false if a child process is created
+            Process() : m_isDetached(true), m_isWaitingDone(false) {}
             
         private:
             bool m_isDetached;
+            bool m_isWaitingDone;
             //...
         };
     }
